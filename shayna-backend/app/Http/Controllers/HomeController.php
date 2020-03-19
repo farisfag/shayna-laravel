22552a4103e515ba25819149cb5855ea42c000d7
Faris Adam <farisadam97@gmail.com>
2020-03-19 16:36:33 +0700

    model added

--- shayna-backend/app/Http/Controllers/HomeController.php
+++ shayna-backend/app/Http/Controllers/HomeController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class HomeController extends Controller
 {
-    //
     /**
      * Create a new controller instance.
      *
@@ -16,9 +15,14 @@ class DashboardController extends Controller
     {
         $this->middleware('auth');
     }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
-        # code...
-        return view('pages.dashboard');
+        return view('home');
     }
 }
--- shayna-backend/routes/web.php
+++ shayna-backend/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'DashboardController@index');
 
 
+
+Auth::routes(['register' => false]);
